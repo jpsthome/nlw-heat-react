d9ff3d3ef8056ab0d02b11a457734cf6a8ff90ba
Joao Pedro <joao.thome@unila.edu.br>
2021-10-19 21:27:44 -0300

    criando componentes com dados estáticos da tela inicial

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,43 @@
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/logo.svg';
+
 export function MessageList(){
 	return (
-		<h1>MessageList</h1>
+		<div className={styles.messageListWrapper}> 
+			<img src={logoImg} alt="DoWhile 2021"/>
+
+			<ul className={styles.messageList}>
+				<li className={styles.message}>
+					<p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+					<div className={styles.messageUser}>
+						<div className={styles.userImage}>
+							<img src="https://github.com/jpsthome.png" alt="João Pedro Thomé" />
+						</div>
+						<span>João Pedro Thomé</span>
+					</div>
+				</li>
+
+				<li className={styles.message}>
+					<p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+					<div className={styles.messageUser}>
+						<div className={styles.userImage}>
+							<img src="https://github.com/jpsthome.png" alt="João Pedro Thomé" />
+						</div>
+						<span>João Pedro Thomé</span>
+					</div>
+				</li>
+
+				<li className={styles.message}>
+					<p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+					<div className={styles.messageUser}>
+						<div className={styles.userImage}>
+							<img src="https://github.com/jpsthome.png" alt="João Pedro Thomé" />
+						</div>
+						<span>João Pedro Thomé</span>
+					</div>
+				</li>
+			</ul>
+		</div>
 	)
 }
